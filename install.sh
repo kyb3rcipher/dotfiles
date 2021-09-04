592cd943010499b80aef719e3e91dd1e73846cef
Kyb3r Vizsla <me@kyb3rvizsla.com>
2021-09-03 19:17:27 -0500

    BSPWM config deleted, README & zsh config updated

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 ## Installing dotfiles
 cp .zshrc .p10k.zsh .gitconfig ~/
-cp -r .config/* ~/.config
 # Creating folder wallpapers
 mkdir ~/.wallpapers
 # Installing Powerlevel10k
@@ -17,14 +16,3 @@ mv sudo.plugin.zsh zsh-sudo.zsh
 git clone https://github.com/gpakosz/.tmux.git ~/.tmux
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
-# Installing Fonts
-mkdir ~/.local/share/fonts
-cd ~/.local/share/fonts
-## Installing Fira Nerd Font
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-unzip FiraCode.zip
-rm FiraCode.zip
-## Installing Polybar Fonts
-git clone https://github.com/adi1090x/polybar-themes.git
-cp -r polybar-themes/fonts/* .
-rm -rf polybar-themes
