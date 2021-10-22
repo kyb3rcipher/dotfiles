51d269b3836743a28c93b29b659bda0d670ebc92
Kyb3r Vizsla <me@kyb3rvizsla.com>
2021-10-21 20:29:15 -0500

    Change in alias: su,snap & new alias ifconfign

--- .aliases.sh
+++ .aliases.sh
@@ -8,6 +8,7 @@ alias kittyo='/usr/bin/bat'
 alias kittyn='/usr/bin/cat'
 alias less='bat --pager=never'
 alias ifconfig="echo 'WTF, use ip a not ifconfig!!!'"
+alias ifconfign="/usr/bin/ifconfig"
 
 # Shortcuts
 alias update='sudo dnf update -y'
@@ -16,7 +17,7 @@ alias mkw="mkdir {scans,informations,exploits,others}"
 alias sd='shutdown now'
 #git
 alias gc='git add .;git commit -m'
-alias gca='git commit -m'
+alias gcc='git commit -m'
 alias gp='git pull'
 alias gps='git push'
 alias gl='git log'
@@ -31,11 +32,11 @@ alias pacman='pacman --color=auto'
 alias apt='sudo apt'
 alias dnf='sudo dnf'
 alias yum='sudo yum'
-alias snap='sudo sna'
+alias snap='sudo snap'
 alias pacman='sudo pacman'
 
 # Others
-alias su='sudo -i'
+alias su='sudo su'
 alias vim='nvim'
 alias vi='nvim'
 alias nano='nvim'
