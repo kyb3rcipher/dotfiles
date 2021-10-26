094017b165bdbae9be97cceb4712cadade75d6b9
Kyb3r Vizsla <me@kyb3rvizsla.com>
2021-10-26 11:22:25 -0500

    Update install script for hacking workspace

--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 ## Installing dotfiles
-cp .zshrc .p10k.zsh .gitconfig ~/
-# Creating folder wallpapers
-mkdir ~/.wallpapers
+cp .zshrc .p10k.zsh .gitconfig .vimrc ~/
 # Installing Powerlevel10k
 git clone https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
 # Installing sudo zsh plugin
-cd ~/.local/share
-mkdir zsh-sudo
-cd zsh-sudo
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
-mv sudo.plugin.zsh zsh-sudo.zsh
+mkdir ~/.local/share/zsh-sudo
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh -O ~/.local/share/zsh-sudo/zsh-sudo.zsh
