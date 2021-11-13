d2976dfe07580e052773f3da775bbb0632588ef0
Kyb3r Vizsla <me@kyb3rvizsla.com>
2021-11-12 23:52:12 -0600

    .vimrc removed :fire: Fixing .aliases.sh :bug:

--- .aliases.sh
+++ .aliases.sh
@@ -8,20 +8,12 @@ alias kittyo='/usr/bin/bat'
 alias kittyn='/usr/bin/cat'
 alias less='bat --pager=never'
 alias ifconfig="echo 'WTF, use ip a not ifconfig!!!'"
-alias ifconfign="/usr/bin/ifconfig"
+alias ifconfign="/usr/sbin/ifconfig"
 
 # Shortcuts
 alias update='sudo apt update && sudo apt upgrade -y'
 alias rmdir='rm -rf'
-alias mkw="mkdir {scans,informations,exploits,others}"
-alias sd='shutdown now'
-#git
-alias gc='git add .;git commit -m'
-alias gcc='git commit -m'
-alias gp='git pull'
-alias gps='git push'
-alias gl='git log'
-alias gck='git checkout'
+alias mkw='mkdir {scans,informations,exploits,others}'
 
 ### Colorize commands
 alias ip='ip --color=auto'
