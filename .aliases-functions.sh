085ab539a224170eacff18744b848102dea8a9d3
Kyb3r Cipher <me@kyb3rcipher.com>
2021-12-13 21:17:24 -0600

    SHELL: .aliases-functions Check lsd,bat and neovim created SHELL: .aliases-functions Some lines were removed

--- .aliases-functions.sh
+++ .aliases-functions.sh
@@ -1,33 +1,23 @@
-alias ls='lsd'
 alias ll='ls -l'
 alias la='ls -a'
-alias cat='bat --pager=never'
-alias catn='bat'
-alias tree='lsd --tree'
+alias lla='ls -la'
+# Check if exist bat and lsd
+if [ -f /usr/bin/lsd ]; then 
+	alias ls='lsd --group-dirs=first'
+	alias tree='lsd --tree'
+fi
+if [ -f /usr/bin/bat ]; then
+	alias cat='bat --page=never'
+fi
 
 # Shortcuts
 alias update='sudo apt update && sudo apt upgrade -y'
 alias rmdir='rm -rf'
-#git
-alias g='git'
-alias ginit='git init && git add . && git commit -m "Initial commit"'
-alias gc='git add . && git commit -m'
-alias gp='git push origin'
-alias gpl='git pull origin'
-
-### Colorize commands
-alias ip='ip --color=auto'
-alias pacman='pacman --color=auto'
-
-### Without sudo commands
-#packages managers
 alias apt='sudo apt'
-alias dpkg='sudo dpkg'
-alias dnf='sudo dnf'
-alias yum='sudo yum'
-alias snap='sudo snap'
-alias pacman='sudo pacman'
+alias su='sudo su'
 
 # Others
-alias su='sudo su'
-alias vim='nvim'
+alias ip='ip --color=auto'
+if [ -f usr/bin/nvim ]; then
+	alias vim='nvim'
+fi
